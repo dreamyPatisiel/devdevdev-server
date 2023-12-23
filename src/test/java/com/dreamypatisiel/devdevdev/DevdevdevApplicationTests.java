2670fabbd2e70fb9dabf22855160915bc1e495a2
yu-so-young2 <sy980911@gmail.com>
2023-12-23 10:51:37 +0900

    feat(프로젝트 생성)

---
+++ src/test/java/com/dreamypatisiel/devdevdev/DevdevdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamypatisiel.devdevdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevdevdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
