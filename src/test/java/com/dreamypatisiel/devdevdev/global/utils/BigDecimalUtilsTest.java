f48870913d17081f83d18e27c0c0911022186878
howisitgoing <howisitgoing@kakao.com>
2024-03-07 01:18:51 +0900

    fix(PickRepositoryImpl): PR Comment 반영
    - PR Comment 반영

--- src/test/java/com/dreamypatisiel/devdevdev/global/utils/BigDecimalUtilsTest.java
+++ src/test/java/com/dreamypatisiel/devdevdev/global/utils/BigDecimalUtilsTest.java
@@ -24,6 +24,19 @@ class BigDecimalUtilsTest {
     }
 
     @Test
+    @DisplayName("퍼센트를 계산할 때 0으로 나누면 0이 반환된다.")
+    void toPercentageOfTotalZero() {
+        BigDecimal value = BigDecimal.valueOf(5);
+        BigDecimal total = BigDecimal.valueOf(0);
+
+        // when
+        BigDecimal result = BigDecimalUtils.toPercentageOf(value, total);
+
+        // then
+        assertThat(result.toBigInteger()).isEqualTo(0);
+    }
+
+    @Test
     @DisplayName("퍼센트로 값을 계산한다.")
     void percentOf() {
         // given
@@ -35,5 +48,19 @@ class BigDecimalUtilsTest {
 
         // then
         assertThat(result.toBigInteger()).isEqualTo(50);
+    }
+
+    @Test
+    @DisplayName("퍼센트로 값을 계산할 때 0으로 나누면 0이 반환된다.")
+    void percentOfTotalZero() {
+        // given
+        BigDecimal percent = BigDecimal.valueOf(10);
+        BigDecimal total = BigDecimal.valueOf(0);
+
+        // when
+        BigDecimal result = BigDecimalUtils.percentOf(percent, total);
+
+        // then
+        assertThat(result.toBigInteger()).isEqualTo(0);
     }
 }
