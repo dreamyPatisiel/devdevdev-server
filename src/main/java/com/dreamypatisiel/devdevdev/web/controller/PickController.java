044a24443503a1c04a90630593d717a9b7694ec6
howisitgoing <howisitgoing@kakao.com>
2024-04-11 23:38:35 +0900

    feat(PickService): 픽픽픽 수정
    - 픽픽픽 수정 서비스 테스트 코드 보충

--- src/main/java/com/dreamypatisiel/devdevdev/web/controller/PickController.java
+++ src/main/java/com/dreamypatisiel/devdevdev/web/controller/PickController.java
@@ -3,10 +3,12 @@ package com.dreamypatisiel.devdevdev.web.controller;
 import com.dreamypatisiel.devdevdev.domain.repository.pick.PickSort;
 import com.dreamypatisiel.devdevdev.domain.service.pick.PickService;
 import com.dreamypatisiel.devdevdev.domain.service.pick.PickServiceStrategy;
+import com.dreamypatisiel.devdevdev.domain.service.response.PickModifyResponse;
 import com.dreamypatisiel.devdevdev.domain.service.response.PickRegisterResponse;
 import com.dreamypatisiel.devdevdev.domain.service.response.PickUploadImageResponse;
 import com.dreamypatisiel.devdevdev.domain.service.response.PicksResponse;
 import com.dreamypatisiel.devdevdev.global.utils.AuthenticationMemberUtils;
+import com.dreamypatisiel.devdevdev.web.controller.request.ModifyPickRequest;
 import com.dreamypatisiel.devdevdev.web.controller.request.RegisterPickRequest;
 import com.dreamypatisiel.devdevdev.web.response.BasicResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -23,8 +25,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -86,6 +90,20 @@ public class PickController {
 
         PickService pickService = pickServiceStrategy.getPickService();
         PickRegisterResponse response = pickService.registerPick(registerPickRequest, authentication);
+
+        return ResponseEntity.ok(BasicResponse.success(response));
+    }
+
+    @Operation(summary = "픽픽픽 수정", description = "픽픽픽을 수정합니다.")
+    @PatchMapping("/picks/{pickId}")
+    public ResponseEntity<BasicResponse<PickModifyResponse>> modifyPick(
+            @PathVariable Long pickId,
+            @RequestBody @Validated ModifyPickRequest modifyPickRequest) {
+
+        Authentication authentication = AuthenticationMemberUtils.getAuthentication();
+
+        PickService pickService = pickServiceStrategy.getPickService();
+        PickModifyResponse response = pickService.modifyPick(pickId, modifyPickRequest, authentication);
 
         return ResponseEntity.ok(BasicResponse.success(response));
     }
