2d489a26a05fa35fd0ccef0cc8b83440607a50a8
yu-so-young2 <sy980911@gmail.com>
2024-06-18 02:00:31 +0900

    test(keyword): 검색어 자동완성 테스트코드

---
+++ src/test/java/com/dreamypatisiel/devdevdev/elastic/domain/service/ElasticKeywordServiceTest.java
@@ -0,0 +1,94 @@
+package com.dreamypatisiel.devdevdev.elastic.domain.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.dreamypatisiel.devdevdev.elastic.domain.document.ElasticKeyword;
+import com.dreamypatisiel.devdevdev.elastic.domain.repository.ElasticKeywordRepository;
+import java.io.IOException;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticKeywordServiceTest {
+
+    @Autowired
+    ElasticKeywordService elasticKeywordService;
+    @Autowired
+    ElasticKeywordRepository elasticKeywordRepository;
+
+    @AfterEach
+    void afterEach() {
+        elasticKeywordRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("검색어와 prefix가 일치하는 키워드를 조회한다.")
+    void autocompleteKeyword() throws IOException {
+        // given
+        ElasticKeyword keyword1 = ElasticKeyword.create(List.of("자바", "Java"));
+        ElasticKeyword keyword2 = ElasticKeyword.create(List.of("자바스크립트", "JavaScript"));
+        ElasticKeyword keyword3 = ElasticKeyword.create(List.of("스프링", "Spring"));
+        ElasticKeyword keyword4 = ElasticKeyword.create(List.of("스프링부트", "SpringBoot"));
+        ElasticKeyword keyword5 = ElasticKeyword.create(List.of("챗지피티", "ChatGPT"));
+        List<ElasticKeyword> elasticKeywords = List.of(keyword1, keyword2, keyword3, keyword4, keyword5);
+        elasticKeywordRepository.saveAll(elasticKeywords);
+
+        String prefix = "자바";
+
+        // when
+        List<String> keywords = elasticKeywordService.autocompleteKeyword(prefix);
+
+        // then
+        assertThat(keywords)
+                .hasSize(2)
+                .contains("자바", "자바스크립트");
+    }
+
+    @Test
+    @DisplayName("영어 대소문자 상관없이 키워드를 조회한다.")
+    void autocompleteKeywordRegardlessOfAlphaCase() throws IOException {
+        // given
+        ElasticKeyword keyword1 = ElasticKeyword.create(List.of("자바", "Java"));
+        ElasticKeyword keyword2 = ElasticKeyword.create(List.of("자바스크립트", "JavaScript"));
+        ElasticKeyword keyword3 = ElasticKeyword.create(List.of("스프링", "Spring"));
+        ElasticKeyword keyword4 = ElasticKeyword.create(List.of("스프링부트", "SpringBoot"));
+        ElasticKeyword keyword5 = ElasticKeyword.create(List.of("챗지피티", "ChatGPT"));
+        List<ElasticKeyword> elasticKeywords = List.of(keyword1, keyword2, keyword3, keyword4, keyword5);
+        elasticKeywordRepository.saveAll(elasticKeywords);
+
+        String prefix = "spr";
+
+        // when
+        List<String> keywords = elasticKeywordService.autocompleteKeyword(prefix);
+
+        // then
+        assertThat(keywords)
+                .hasSize(2)
+                .contains("Spring", "SpringBoot");
+    }
+
+    @Test
+    @DisplayName("일치하는 키워드가 없을 경우 빈 리스트를 반환한다.")
+    void autocompleteKeywordNotFound() throws IOException {
+        // given
+        ElasticKeyword keyword1 = ElasticKeyword.create(List.of("자바", "Java"));
+        ElasticKeyword keyword2 = ElasticKeyword.create(List.of("자바스크립트", "JavaScript"));
+        ElasticKeyword keyword3 = ElasticKeyword.create(List.of("스프링", "Spring"));
+        ElasticKeyword keyword4 = ElasticKeyword.create(List.of("스프링부트", "SpringBoot"));
+        ElasticKeyword keyword5 = ElasticKeyword.create(List.of("챗지피티", "ChatGPT"));
+        List<ElasticKeyword> elasticKeywords = List.of(keyword1, keyword2, keyword3, keyword4, keyword5);
+        elasticKeywordRepository.saveAll(elasticKeywords);
+
+        String prefix = "엘라스틱서치";
+
+        // when
+        List<String> keywords = elasticKeywordService.autocompleteKeyword(prefix);
+
+        // then
+        assertThat(keywords).isEmpty();
+    }
+}
---
+++ src/test/java/com/dreamypatisiel/devdevdev/web/controller/KeywordControllerTest.java
@@ -0,0 +1,88 @@
+package com.dreamypatisiel.devdevdev.web.controller;
+
+import static com.dreamypatisiel.devdevdev.global.constant.SecurityConstant.AUTHORIZATION_HEADER;
+import static com.dreamypatisiel.devdevdev.global.constant.SecurityConstant.BEARER_PREFIX;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.dreamypatisiel.devdevdev.domain.entity.Member;
+import com.dreamypatisiel.devdevdev.domain.entity.enums.Role;
+import com.dreamypatisiel.devdevdev.domain.entity.enums.SocialType;
+import com.dreamypatisiel.devdevdev.domain.repository.member.MemberRepository;
+import com.dreamypatisiel.devdevdev.elastic.domain.document.ElasticKeyword;
+import com.dreamypatisiel.devdevdev.elastic.domain.repository.ElasticKeywordRepository;
+import com.dreamypatisiel.devdevdev.elastic.domain.service.ElasticKeywordService;
+import com.dreamypatisiel.devdevdev.global.security.oauth2.model.SocialMemberDto;
+import com.dreamypatisiel.devdevdev.web.response.ResultType;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+
+class KeywordControllerTest extends SupportControllerTest {
+
+    @Autowired
+    ElasticKeywordService elasticKeywordService;
+    @Autowired
+    ElasticKeywordRepository elasticKeywordRepository;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @AfterEach
+    void afterEach() {
+        elasticKeywordRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("기술블로그 키워드를 검색하면 자동완성 키워드 후보 리스트를 최대 20개 반환한다.")
+    void autocompleteKeyword() throws Exception {
+        // given
+        SocialMemberDto socialMemberDto = createSocialDto("dreamy5patisiel", "꿈빛파티시엘",
+                "꿈빛파티시엘", "1234", email, socialType, role);
+        Member member = Member.createMemberBy(socialMemberDto);
+        member.updateRefreshToken(refreshToken);
+        memberRepository.save(member);
+
+        ElasticKeyword keyword1 = ElasticKeyword.create(List.of("자바", "Java"));
+        ElasticKeyword keyword2 = ElasticKeyword.create(List.of("자바스크립트", "JavaScript"));
+        ElasticKeyword keyword3 = ElasticKeyword.create(List.of("자바가 최고야", "Java is the best"));
+        ElasticKeyword keyword4 = ElasticKeyword.create(List.of("스프링", "Spring"));
+        ElasticKeyword keyword5 = ElasticKeyword.create(List.of("스프링부트", "SpringBoot"));
+        List<ElasticKeyword> elasticKeywords = List.of(keyword1, keyword2, keyword3, keyword4, keyword5);
+        elasticKeywordRepository.saveAll(elasticKeywords);
+
+        String prefix = "자";
+
+        // when // then
+        ResultActions actions = mockMvc.perform(get(DEFAULT_PATH_V1 + "/keywords/auto-complete")
+                        .queryParam("prefix", prefix)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .header(AUTHORIZATION_HEADER, BEARER_PREFIX + accessToken))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.resultType").value(ResultType.SUCCESS.name()))
+                .andExpect(jsonPath("$.datas").isArray())
+                .andExpect(jsonPath("$.datas").isNotEmpty());
+    }
+
+    private SocialMemberDto createSocialDto(String userId, String name, String nickName, String password, String email,
+                                            String socialType, String role) {
+        return SocialMemberDto.builder()
+                .userId(userId)
+                .name(name)
+                .nickname(nickName)
+                .password(password)
+                .email(email)
+                .socialType(SocialType.valueOf(socialType))
+                .role(Role.valueOf(role))
+                .build();
+    }
+
+}
