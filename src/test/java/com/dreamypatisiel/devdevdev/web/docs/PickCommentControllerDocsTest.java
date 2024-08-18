74a3d4569a4835cfa0eb05adc16eddc20e978bfc
howisitgoing <howisitgoing@kakao.com>
2024-08-18 16:44:02 +0900

    docs(PickCommentControllerDocsTest): deletePickReply
    - 픽픽픽 답글 삭제 문서 작성

--- src/test/java/com/dreamypatisiel/devdevdev/web/docs/PickCommentControllerDocsTest.java
+++ src/test/java/com/dreamypatisiel/devdevdev/web/docs/PickCommentControllerDocsTest.java
@@ -698,6 +698,118 @@ public class PickCommentControllerDocsTest extends SupportControllerDocsTest {
         ));
     }
 
+    @Test
+    @DisplayName("회원이 승인 상태의 픽픽픽 댓글의 본인이 작성한 삭제되지 않은 답글을 삭제한다.")
+    void deletePickReply() throws Exception {
+        // given
+        // 회원 생성
+        SocialMemberDto socialMemberDto = createSocialDto("dreamy5patisiel", "꿈빛파티시엘",
+                "꿈빛파티시엘", "1234", email, socialType, role);
+        Member member = Member.createMemberBy(socialMemberDto);
+        memberRepository.save(member);
+
+        // 픽픽픽 작성자 생성
+        SocialMemberDto authorSocialMemberDto = createSocialDto("authorId", "author",
+                "꿈빛맛티시엘", "1234", "authorDreamy5patisiel@kakao.com", socialType, role);
+        Member author = Member.createMemberBy(authorSocialMemberDto);
+        memberRepository.save(author);
+
+        // 픽픽픽 생성
+        Pick pick = createPick(new Title("픽픽픽 타이틀"), ContentStatus.APPROVAL, author);
+        pickRepository.save(pick);
+
+        // 픽픽픽 댓글 생성
+        PickComment pickComment = createPickComment(new CommentContents("안녕하세웅 댓글"), false, member, pick);
+        pickCommentRepository.save(pickComment);
+
+        // 픽픽픽 답글 생성
+        PickReply pickReply = createPickReply(new CommentContents("안녕하세웅 답글"), member, pickComment);
+        pickReplyRepository.save(pickReply);
+
+        // when // then
+        ResultActions actions = mockMvc.perform(delete(
+                        "/devdevdev/api/v1/picks/{pickId}/comments/{pickCommentId}/replies/{pickReplyId}",
+                        pick.getId(), pickComment.getId(), pickReply.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(AUTHORIZATION_HEADER, SecurityConstant.BEARER_PREFIX + accessToken)
+                        .characterEncoding(StandardCharsets.UTF_8))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        // docs
+        actions.andDo(document("delete-pick-reply",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestHeaders(
+                        headerWithName(AUTHORIZATION_HEADER).description("Bearer 엑세스 토큰")
+                ),
+                pathParameters(
+                        parameterWithName("pickId").description("픽픽픽 아이디"),
+                        parameterWithName("pickCommentId").description("픽픽픽 댓글 아이디"),
+                        parameterWithName("pickReplyId").description("픽픽픽 답글 아이디")
+                ),
+                responseFields(
+                        fieldWithPath("resultType").type(STRING).description("응답 결과"),
+                        fieldWithPath("data").type(OBJECT).description("응답 데이터"),
+                        fieldWithPath("data.pickReplyId").type(NUMBER).description("픽픽픽 답글 아이디")
+                )
+        ));
+    }
+
+    @Test
+    @DisplayName("픽픽픽 답글을 삭제할 때 본인이 작성한 답글이 아니면 예외가 발생한다.")
+    void deletePickReplyNotFoundException() throws Exception {
+        // given
+        // 회원 생성
+        SocialMemberDto socialMemberDto = createSocialDto("dreamy5patisiel", "꿈빛파티시엘",
+                "꿈빛파티시엘", "1234", email, socialType, role);
+        Member member = Member.createMemberBy(socialMemberDto);
+        memberRepository.save(member);
+
+        // 픽픽픽 작성자 생성
+        SocialMemberDto authorSocialMemberDto = createSocialDto("authorId", "author",
+                "꿈빛맛티시엘", "1234", "authorDreamy5patisiel@kakao.com", socialType, role);
+        Member author = Member.createMemberBy(authorSocialMemberDto);
+        memberRepository.save(author);
+
+        // 픽픽픽 생성
+        Pick pick = createPick(new Title("픽픽픽 타이틀"), ContentStatus.APPROVAL, author);
+        pickRepository.save(pick);
+
+        // 픽픽픽 댓글 생성
+        PickComment pickComment = createPickComment(new CommentContents("안녕하세웅 댓글"), false, member, pick);
+        pickCommentRepository.save(pickComment);
+
+        // 픽픽픽 답글 생성
+        PickReply pickReply = createPickReply(new CommentContents("안녕하세웅 답글"), author, pickComment);
+        pickReplyRepository.save(pickReply);
+
+        // when // then
+        ResultActions actions = mockMvc.perform(delete(
+                        "/devdevdev/api/v1/picks/{pickId}/comments/{pickCommentId}/replies/{pickReplyId}",
+                        pick.getId(), pickComment.getId(), pickReply.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(AUTHORIZATION_HEADER, SecurityConstant.BEARER_PREFIX + accessToken)
+                        .characterEncoding(StandardCharsets.UTF_8))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+
+        // docs
+        actions.andDo(document("delete-pick-reply-not-found-exception",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestHeaders(
+                        headerWithName(AUTHORIZATION_HEADER).description("Bearer 엑세스 토큰")
+                ),
+                pathParameters(
+                        parameterWithName("pickId").description("픽픽픽 아이디"),
+                        parameterWithName("pickCommentId").description("픽픽픽 댓글 아이디"),
+                        parameterWithName("pickReplyId").description("픽픽픽 답글 아이디")
+                ),
+                exceptionResponseFields()
+        ));
+    }
+
     private PickReply createPickReply(CommentContents commentContents, Member member, PickComment pickComment) {
         return PickReply.builder()
                 .contents(commentContents)
