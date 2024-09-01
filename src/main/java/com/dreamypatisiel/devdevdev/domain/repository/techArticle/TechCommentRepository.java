9f13eb2c30df7d938ebcdefcd5302f1e60b13cc5
soyoung <sy980911@gmail.com>
2024-09-02 01:26:11 +0900

    refactor(techComment): 엔티티그래프 추가

--- src/main/java/com/dreamypatisiel/devdevdev/domain/repository/techArticle/TechCommentRepository.java
+++ src/main/java/com/dreamypatisiel/devdevdev/domain/repository/techArticle/TechCommentRepository.java
@@ -2,14 +2,19 @@ package com.dreamypatisiel.devdevdev.domain.repository.techArticle;
 
 import com.dreamypatisiel.devdevdev.domain.entity.TechComment;
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TechCommentRepository extends JpaRepository<TechComment, Long> {
 
+    @EntityGraph(attributePaths = {"techArticle"})
     Optional<TechComment> findWithTechArticleByIdAndTechArticleIdAndCreatedByIdAndDeletedAtIsNull(Long id, Long techArticleId,
                                                                                    Long createdById);
 
+    @EntityGraph(attributePaths = {"techArticle"})
     Optional<TechComment> findWithTechArticleByIdAndTechArticleIdAndDeletedAtIsNull(Long id, Long techArticleId);
 
+    @EntityGraph(attributePaths = {"techArticle"})
     Optional<TechComment> findWithTechArticleByIdAndTechArticleId(Long id, Long techArticleId);
 }
