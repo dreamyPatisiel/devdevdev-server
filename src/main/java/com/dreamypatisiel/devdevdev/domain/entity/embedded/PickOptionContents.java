ae6c17066d47b1a689406c7b5d4fdb625ef7afb1
howisitgoing <howisitgoing@kakao.com>
2024-09-06 22:05:44 +0900

    fix(PR 반영):
    - DP-363 머지
    - 테스트 코드 수정

--- src/main/java/com/dreamypatisiel/devdevdev/domain/entity/embedded/PickOptionContents.java
+++ src/main/java/com/dreamypatisiel/devdevdev/domain/entity/embedded/PickOptionContents.java
@@ -6,7 +6,7 @@ import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 
 @Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -19,23 +19,23 @@ public class PickOptionContents {
     private String pickOptionContents;
 
     public PickOptionContents(String pickOptionContents) {
-        validationTopicContents(pickOptionContents);
-        this.pickOptionContents = pickOptionContents;
+        // 빈문자열 이거나 null 이면 null 로 저장해야한다.
+        if (!StringUtils.hasText(pickOptionContents)) {
+            this.pickOptionContents = null;
+        } else {
+            validationTopicContents(pickOptionContents);
+            this.pickOptionContents = pickOptionContents;
+        }
     }
 
     private void validationTopicContents(String pickOptionContents) {
-        // null을 허용한다.
-        if (pickOptionContents == null) {
-            return;
-        }
-
         if (!isValidLength(pickOptionContents)) {
             throw new TopicContentsException(getInvalidLengthExceptionMessage());
         }
     }
 
     private static boolean isValidLength(String pickOptionContents) {
-        return !ObjectUtils.isEmpty(pickOptionContents) && pickOptionContents.length() <= MAX_PICK_CONTENTS_LENGTH;
+        return pickOptionContents.length() <= MAX_PICK_CONTENTS_LENGTH;
     }
 
     public static String getInvalidLengthExceptionMessage() {
