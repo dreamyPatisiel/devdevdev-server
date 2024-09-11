cecd861b0df275be6619a4d77e8af19c2acde9ac
howisitgoing <howisitgoing@kakao.com>
2024-09-11 20:23:47 +0900

    feat(BlameController): 신고 사유 조회
    - 신고 사유 조회 컨트롤러 개발 및 테스트 코드 작성

--- src/main/java/com/dreamypatisiel/devdevdev/web/controller/BlameController.java
+++ src/main/java/com/dreamypatisiel/devdevdev/web/controller/BlameController.java
@@ -5,13 +5,14 @@ import com.dreamypatisiel.devdevdev.web.dto.response.BasicResponse;
 import com.dreamypatisiel.devdevdev.web.dto.response.common.BlameTypeResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "픽픽픽 신고 API", description = "픽픽픽 신고")
+@Tag(name = "댑댑댑 신고 API", description = "댑댑댑 신고 기능")
 @RestController
 @RequestMapping("/devdevdev/api/v1")
 @RequiredArgsConstructor
@@ -21,7 +22,9 @@ public class BlameController {
 
     @Operation(summary = "신고 사유 조회", description = "회원은 신고 사유를 조회합니다.")
     @GetMapping("/blames")
-    public ResponseEntity<BasicResponse<BlameTypeResponse>> findBlames() {
-        return ResponseEntity.ok(BasicResponse.success(memberBlameService.findBlameType()));
+    public ResponseEntity<BasicResponse<BlameTypeResponse>> getBlames() {
+        List<BlameTypeResponse> response = memberBlameService.findBlameType();
+        
+        return ResponseEntity.ok(BasicResponse.success(response));
     }
 }
---
+++ src/test/java/com/dreamypatisiel/devdevdev/web/controller/BlameControllerTest.java
@@ -0,0 +1,70 @@
+package com.dreamypatisiel.devdevdev.web.controller;
+
+import static com.dreamypatisiel.devdevdev.web.dto.response.ResultType.SUCCESS;
+import static io.lettuce.core.BitFieldArgs.OverflowType.FAIL;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.dreamypatisiel.devdevdev.domain.entity.BlameType;
+import com.dreamypatisiel.devdevdev.domain.repository.BlameRepository;
+import com.dreamypatisiel.devdevdev.domain.repository.BlameTypeRepository;
+import com.dreamypatisiel.devdevdev.global.constant.SecurityConstant;
+import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+
+public class BlameControllerTest extends SupportControllerTest {
+
+    @Autowired
+    BlameTypeRepository blameTypeRepository;
+    @Autowired
+    BlameRepository blameRepository;
+
+
+    @Test
+    @DisplayName("회원이 신고사유를 조회한다.")
+    void getBlames() throws Exception {
+        // given
+        BlameType blameType = createBlameType("욕설1", 0);
+        blameTypeRepository.save(blameType);
+
+        // when // then
+        mockMvc.perform(get("/devdevdev/api/v1/blames")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .header(SecurityConstant.AUTHORIZATION_HEADER, SecurityConstant.BEARER_PREFIX + accessToken))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.resultType").value(SUCCESS.name()))
+                .andExpect(jsonPath("$.datas").isNotEmpty())
+                .andExpect(jsonPath("$.datas.[0].id").isNumber())
+                .andExpect(jsonPath("$.datas.[0].reason").isString())
+                .andExpect(jsonPath("$.datas.[0].sortOrder").isNumber());
+    }
+
+    @Test
+    @DisplayName("회원이 아니면 신고사유를 조회할 때 예외가 발생한다.")
+    void getBlamesException() throws Exception {
+        // given
+        BlameType blameType = createBlameType("욕설1", 0);
+        blameTypeRepository.save(blameType);
+
+        // when // then
+        mockMvc.perform(get("/devdevdev/api/v1/blames")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.resultType").value(FAIL.name()))
+                .andExpect(jsonPath("$.message").isString())
+                .andExpect(jsonPath("$.errorCode").isNumber());
+    }
+
+    private BlameType createBlameType(String reason, int sortOrder) {
+        return new BlameType(reason, sortOrder);
+    }
+}
