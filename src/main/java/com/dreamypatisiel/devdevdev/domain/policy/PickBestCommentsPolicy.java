4f20264c0e3285e33966be54a1d7809af404ea63
howisitgoing <howisitgoing@kakao.com>
2024-10-17 00:36:29 +0900

    fix(PR): 리뷰 반영

--- src/main/java/com/dreamypatisiel/devdevdev/domain/policy/PickBestCommentsPolicy.java
+++ src/main/java/com/dreamypatisiel/devdevdev/domain/policy/PickBestCommentsPolicy.java
@@ -9,12 +9,12 @@ public class PickBestCommentsPolicy implements BestCommentPolicy {
     public static final int BEST_COMMENTS_DEFAULT_OFFSET = 3;
 
     /**
-     * @Note: 베스트 댓글 size 정책을 적용하여 예측 불가능한 자원을 낭비한다.(최소 3개 최대 10개)
+     * @Note: 베스트 댓글 size 정책을 적용하여 예측 불가능한 자원 낭비를 방지한다.(최소 3개 최대 10개)
      * @Author: 장세웅
      * @Since: 2024.10.09
      */
     public int applySize(int size) {
-        if (size > BEST_COMMENTS_MAX_OFFSET) {
+        if (size > BEST_COMMENTS_MAX_OFFSET || size < BEST_COMMENTS_DEFAULT_OFFSET) {
             return BEST_COMMENTS_DEFAULT_OFFSET;
         }
 
