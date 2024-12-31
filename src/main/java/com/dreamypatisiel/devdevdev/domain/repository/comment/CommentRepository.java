f8d2da8018b73300b673b42162d0d494967da6c2
howisitgoing <howisitgoing@kakao.com>
2024-12-31 16:02:37 +0900

    feat(CommentRepository): findMyWrittenCommentsByCursor(..)
    - 내가 썼어요 댓글 쿼리 개발 및 테스트 코드 작성

--- src/main/java/com/dreamypatisiel/devdevdev/domain/repository/comment/CommentRepository.java
+++ src/main/java/com/dreamypatisiel/devdevdev/domain/repository/comment/CommentRepository.java
@@ -17,10 +17,10 @@ public class CommentRepository {
     private final PickCommentRepository pickCommentRepository;
     private final TechCommentRepository techCommentRepository;
 
-    public SliceCustom<MyWrittenComment> findMyWrittenCommentsBy(Long memberId,
-                                                                 Long pickCommentId,
-                                                                 Long techCommentId,
-                                                                 Pageable pageable) {
+    public SliceCustom<MyWrittenComment> findMyWrittenCommentsByCursor(Long memberId,
+                                                                       Long pickCommentId,
+                                                                       Long techCommentId,
+                                                                       Pageable pageable) {
 
         // 회원이 작성한 픽픽픽, 기술블로그 댓글 조회
         List<MyWrittenComment> findMyWrittenComments = commentMapper.findByMemberIdAndPickCommentIdAndTechCommentIdOrderByCommentCreatedAtDesc(
@@ -30,12 +30,12 @@ public class CommentRepository {
         boolean hasNext = findMyWrittenComments.size() >= pageable.getPageSize();
 
         // 회원이 작성한 댓글 총 갯수(삭제 미포함)
-        Long commentTotalCount = countByCreatedByAndDeletedAtIsNull(memberId);
+        Long commentTotalCount = countByCreatedByIdAndDeletedAtIsNull(memberId);
 
         return new SliceCustom<>(findMyWrittenComments, pageable, hasNext, commentTotalCount);
     }
 
-    private Long countByCreatedByAndDeletedAtIsNull(Long createdById) {
+    private Long countByCreatedByIdAndDeletedAtIsNull(Long createdById) {
 
         // 회원이 작성한 픽픽픽 댓글 갯수(삭제 미포함)
         Long pickCommentTotal = pickCommentRepository.countByCreatedByIdAndDeletedAtIsNull(createdById);
