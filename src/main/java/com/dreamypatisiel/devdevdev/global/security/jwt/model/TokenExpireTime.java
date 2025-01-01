638ce24cbbc49b31a586de4fcf62f47181f0aa8c
soyoung <sy980911@gmail.com>
2025-01-01 23:29:18 +0900

    fix: 토큰 만료기간 환경변수화

--- src/main/java/com/dreamypatisiel/devdevdev/global/security/jwt/model/TokenExpireTime.java
+++ src/main/java/com/dreamypatisiel/devdevdev/global/security/jwt/model/TokenExpireTime.java
@@ -1,6 +1,18 @@
 package com.dreamypatisiel.devdevdev.global.security.jwt.model;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TokenExpireTime {
-    public static final long ACCESS_TOKEN_EXPIRE_TIME = 1_000 * 60 * 30; // 30분
-    public static final long REFRESH_TOKEN_EXPIRE_TIME = 1_000 * 60 * 60 * 24 * 7; // 7일
+    public static long ACCESS_TOKEN_EXPIRE_TIME;
+    public static long REFRESH_TOKEN_EXPIRE_TIME;
+
+    public TokenExpireTime(
+            @Value("${token.expire.access}") long accessTokenExpireTime,
+            @Value("${token.expire.refresh}") long refreshTokenExpireTime
+    ) {
+        ACCESS_TOKEN_EXPIRE_TIME = accessTokenExpireTime;
+        REFRESH_TOKEN_EXPIRE_TIME = refreshTokenExpireTime;
+    }
 }
