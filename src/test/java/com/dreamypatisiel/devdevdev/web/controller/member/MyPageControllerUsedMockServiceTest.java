fddc7c9bc424f3545794494825a50f79424bc2c6
soyoung <sy980911@gmail.com>
2025-06-29 18:24:50 +0900

    fix: 테스트코드 및 mypage 하위로 이동

--- src/test/java/com/dreamypatisiel/devdevdev/web/controller/member/MyPageControllerUsedMockServiceTest.java
+++ src/test/java/com/dreamypatisiel/devdevdev/web/controller/member/MyPageControllerUsedMockServiceTest.java
@@ -15,6 +15,7 @@ import com.dreamypatisiel.devdevdev.domain.entity.enums.PickOptionType;
 import com.dreamypatisiel.devdevdev.domain.entity.enums.Role;
 import com.dreamypatisiel.devdevdev.domain.entity.enums.SocialType;
 import com.dreamypatisiel.devdevdev.domain.repository.member.MemberRepository;
+import com.dreamypatisiel.devdevdev.domain.service.member.MemberNicknameDictionaryService;
 import com.dreamypatisiel.devdevdev.domain.service.member.MemberService;
 import com.dreamypatisiel.devdevdev.global.constant.SecurityConstant;
 import com.dreamypatisiel.devdevdev.global.security.oauth2.model.SocialMemberDto;
@@ -44,6 +45,29 @@ public class MyPageControllerUsedMockServiceTest extends SupportControllerTest {
     MemberRepository memberRepository;
     @MockBean
     MemberService memberService;
+    @MockBean
+    MemberNicknameDictionaryService memberNicknameDictionaryService;
+
+    @Test
+    @DisplayName("회원은 랜덤 닉네임을 생성할 수 있다.")
+    void getRandomNickname() throws Exception {
+        // given
+        String result = "주말에 공부하는 토마토";
+
+        // when
+        when(memberNicknameDictionaryService.createRandomNickname()).thenReturn(result);
+
+        // then
+        mockMvc.perform(get("/devdevdev/api/v1/mypage/nickname/random")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .header(SecurityConstant.AUTHORIZATION_HEADER, SecurityConstant.BEARER_PREFIX + accessToken))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.resultType").value(SUCCESS.name()))
+                .andExpect(jsonPath("$.data").isNotEmpty())
+                .andExpect(jsonPath("$.data").isString());
+    }
 
     @Test
     @DisplayName("회원이 내가 썼어요 댓글을 조회한다.")
